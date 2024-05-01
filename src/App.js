e6f52b418ad12baa831ad470db249a5abe9f6baa
DzakirizaDF <ddf7802@gmail.com>
2024-05-02 01:12:54 +0700

    push client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
+import { useEffect, useState } from 'react';
+
+const socket = io.connect('http://localhost:3001'); // Connect to server
+// const socket = io.connect(); //Find a port automatically 
 
 function App() {
+  const [input, setMessages] = useState('');
+  const [receive, receiveMessage] = useState('');
+  const [socket_id, socketId] = useState('');
+  const handleClick = () => {
+    if(socket_id){
+      socket.emit('send_id', {socket_id, input});
+    } else{
+      socket.emit('send_message', {message : input});
+    }
+  }; 
+
+  useEffect(() => { 
+    socket.on('receive_message', (msg) => {
+      // console.log(msg);
+      receiveMessage(msg.message);
+    });
+    socket.on('ngobrol_message', (input) => {
+      // console.log(msg);
+      receiveMessage(input);
+    });
+  }, [socket]);
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +43,18 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <input placeholder='input...' onChange={(event) =>
+        setMessages(event.target.value)
+      }/>
+      <button onClick={handleClick}>Send</button>
+      <br/>
+      {receive}
+
+      <input placeholder='id' onChange={(event) =>
+        socketId(event.target.value)
+      }/>
+      {/* <button onClick={tapButton}>Tap</button> */}
     </div>
   );
 }
